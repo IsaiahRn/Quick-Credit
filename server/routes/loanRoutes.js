513d305661b9e8eb803c280d82b90a0c67180483
IsaiahRn <isaiahrjr7@gmail.com>
2019-05-12 15:20:00 +0200

    [chore 165956674] adding user authentication

--- server/routes/loanRoutes.js
+++ server/routes/loanRoutes.js
@@ -11,18 +11,18 @@ const router = Router();
 router.post('/', isAuth, loanController.createLoan);
 
 // GET api/v1/loans (fetch all created loans)
-router.get('/', loanController.getAllLoans);
+router.get('/', isAuth, loanController.getAllLoans);
 
 // GET api/v1/loans/<loanId> (fetch a single loan)
 router.get('/:loanId', loanController.getLoan);
 
 // PATCH api/v1/loans/<loanId> (Approve Or Reject a loan request)
-router.patch('/:loanId', loanController.getApproveReject);
+router.patch('/:loanId', isAuth, isAdmin, loanController.getApproveReject);
 
 // POST api/v1/loans/<loanId>/repayment (Create a repayment history)
-router.post('/:loanId/repayment', loanController.createRepaymentRecord);
+router.post('/:loanId/repayment', isAuth, isAdmin, loanController.createRepaymentRecord);
 
 // GET api/v1/<loanId>/repayment (fetch a repayment history)
-router.get('/:loanId/repayment', loanController.getRepaymentRecords);
+router.get('/:loanId/repayment', isAuth, loanController.getRepaymentRecords);
 
 export default router;
